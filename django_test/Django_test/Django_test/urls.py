c8dcec02f922c576d6c112feb28c2ad5e7152b7a
leo <leo7412369@gmail.com>
2024-04-26 23:56:44 +0800

    update 4_26_2024

--- django_test/Django_test/Django_test/urls.py
+++ django_test/Django_test/Django_test/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from mytestweb.views import hello_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('hello/', hello_view),
 ]
