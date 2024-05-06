03e1564f39af09fb46ec58957945b69736577e32
youngchenghung <34208765+youngchenghung@users.noreply.github.com>
2024-05-06 14:32:48 +0800

    Update bs4_request.py

--- django_test/Django_test/mytestweb/bs4_request.py
+++ django_test/Django_test/mytestweb/bs4_request.py
@@ -54,7 +54,7 @@ def search_data():
             pandas_dataframe = pd.DataFrame(data)
             print(pandas_dataframe)
         
-        pandas_dataframe.to_csv(r'/Users/leo/Python_Django/django_test/Django_test/PTT_Gossiping_data.csv', 
+        pandas_dataframe.to_csv(r'/Users/kohakudaitoku/Documents/聯成/Python_Django-main/django_test/Django_test/PTT_Gossiping_data.csv', 
                                 encoding='utf-8', 
                                 index=False)
 
@@ -85,13 +85,13 @@ if __name__ == '__main__':
 #%%
 import pandas as pd
 
-df = pd.read_csv('/Users/leo/Python_Django/django_test/Django_test/PTT_Gossiping_data.csv')
+df = pd.read_csv('/Users/kohakudaitoku/Documents/聯成/Python_Django-main/django_test/Django_test/PTT_Gossiping_data.csv')
 select = df[['title','pop']]
 print(select.sort_values(by='pop', ascending=False))
 #%%
 
 # 讀取爬蟲資料
-df = pd.read_csv('/Users/leo/Python_Django/django_test/Django_test/PTT_Gossiping_data.csv')
+df = pd.read_csv('/Users/kohakudaitoku/Documents/聯成/Python_Django-main/django_test/Django_test/PTT_Gossiping_data.csv')
 x = []
 # for index, row in df.iterrows():
 #     title = row['title']
@@ -109,10 +109,10 @@ x = []
 # 迴圈取出title欄位中的資料，並存入 x list中
 for i in df['title']:
     x.append(i)
-# print(x)
+print(x)
 
 # 將 x list中的資料寫入檔案
-with (open('/Users/leo/Python_Django/django_test/Django_test/PTT_title_list.txt', 'w', encoding='utf-8')) as f:
+with (open('/Users/kohakudaitoku/Documents/聯成/Python_Django-main/django_test/Django_test/PTT_title_list.txt', 'w', encoding='utf-8')) as f:
     for i in x:
         f.write(i + '\n')
 
@@ -129,49 +129,109 @@ with (open('/Users/leo/Python_Django/django_test/Django_test/PTT_title_list.txt'
 # print(aaa)
 # sort = aaa.sort_values(by='Pop', ascending=False)
 # print(sort.head(50))
+
+
 # %%
 
 import jieba
 import jieba.analyse
 from collections import Counter
+import pandas as pd
 
 # 讀取 title 資料檔案 title_list.txt
-with (open('/Users/leo/Python_Django/django_test/Django_test/PTT_title_list.txt', 'r', encoding='utf-8')) as f:
+with (open('/Users/kohakudaitoku/Documents/聯成/Python_Django-main/django_test/Django_test/PTT_title_list.txt', 'r', encoding='utf-8')) as f:
     data = f.read()
-    print(data)
+    # print(data)
 
 # 讀取停用字檔案 stopwords.txt
-stop_words_file = '/Users/leo/Python_Django/django_test/Django_test/stopwords.txt'
-
-# 停用字轉串列
+stop_words_file = '/Users/kohakudaitoku/Documents/聯成/Python_Django-main/django_test/Django_test/stop_words.txt'
 stop_words_list = []
-with (open(stop_words_file, 'r', encoding='utf-8')) as f:
-    for line in f:
-        stop_words_list.append(line.replace('\n',''))
+for line in open(stop_words_file, 'r', encoding='utf-8'):
+    stop_words_list.append(line.strip('\n'))
 # print(stop_words_list)
 
-# 將 title_list.txt 中有停用字去除
+# 停用字轉串列格式,並將title資料做停用字過濾
 filter_words = []
-for k in data:
-    if k not in stop_words_list:
-        filter_words.append(k)
-print(filter_words)
+# stop_words_list = stop_words_file
+for word_list in data:
+    if all(filter_words not in word_list for filter_words in stop_words_list):
+        filter_words.append(word_list.strip('\n'))
+# print(filter_words)
 
-words = jieba.cut(data)
-words_list = list(words)
-print(words_list)
+# 將list 轉字串型態
+join_filter_words = ''.join(filter_words)
+# print(join_filter_words)
+# jieba sjieba.analyse.extract_tags() 取出前20個關鍵字
+# keywords = jieba.analyse.extract_tags(join_stop_words, topK=20)
+# print(keywords)
 
-kywords = jieba.analyse.extract_tags(data, topK=20)
-print(kywords)
+# jieba處理字轉詞
+jieba_words = jieba.lcut(join_filter_words)
+# print(jieba_words)
+keywords = []
+for i in jieba_words:
+    # 過濾字當只有一個字
+    if len(i) == 1:
+        continue
+    else:
+        keywords.append(i)
+# print(keywords)
 
-words_count = Counter(words_list)
-print(words_count)
+# 使用 Counter 計算每個詞的出現次數
+keywords_counts = Counter(keywords)
+# print(keywords_counts)
 
-df = pd.DataFrame(list(words_count.items()), columns=['word', 'count'])
+# 將結果存入 DataFrame, 並依照 count 排序
+df = pd.DataFrame(list(keywords_counts.items()), columns=['Word', 'Counts'])
+x = df.sort_values(by='Counts', ascending=False)
+print(x.head(10))
 
-df.to_csv('/Users/leo/Python_Django/django_test/Django_test/PTT_words_count.csv', 
+df.to_csv('/Users/kohakudaitoku/Documents/聯成/Python_Django-main/django_test/Django_test/PTT_words_count.csv', 
           encoding='utf-8', 
           index=False)
 
+with (open('/Users/kohakudaitoku/Documents/聯成/Python_Django-main/django_test/Django_test/PTT_words_count.csv', 'r', encoding='utf-8')) as f:
+    PTT_words_count = pd.read_csv(f)
+    print(PTT_words_count)
 
-# %%
+
+# 排序欄位Counts從大到小
+sort_counts = PTT_words_count.sort_values(by='Counts', ascending=False)
+print(sort_counts)
+
+#%%
+import jieba
+import jieba.analyse
+from collections import Counter
+import pandas as pd
+
+df = pd.read_csv('/Users/kohakudaitoku/Documents/聯成/Python_Django-main/django_test/Django_test/PTT_Gossiping_data.csv')
+print(df)
+
+category_counts =  df.groupby(["category","date"]).size().reset_index(name='category count').sort_values(by='category count', ascending=False)
+category_counts_top10 = category_counts.head(10)
+print(category_counts_top10)
+
+category =  df.groupby(["category"]).size().reset_index(name='category count').sort_values(by='category count', ascending=False)
+category_top10 = category.head(10)
+print(category_top10)
+
+date =  df.groupby(["date"]).size().reset_index(name='date count').sort_values(by='date count', ascending=False)
+date_top10 = date.head(10)
+print(date_top10)
+
+author_counts =  df.groupby("author").size().reset_index(name='author count').sort_values(by='author count', ascending=False)
+author_counts_top10 = author_counts.head(10)
+print(author_counts_top10)
+
+author_counts =  df["author"] == "words2012"
+print(df[author_counts])
+
+author2_counts =  df["author"] == "xzcb2008"
+print(df[author2_counts])
+
+author3_counts =  df["author"] == "felixden"
+print(df[author3_counts])
+
+author4_counts =  df["author"] == "ianlin1216"
+print(df[author4_counts])
