a7bb043068c817e38a90555c11bd08232917f5ee
youngchenghung <34208765+youngchenghung@users.noreply.github.com>
2024-05-07 16:54:05 +0800

    Update test.py

--- django_test/Django_test/mytestweb/test.py
+++ django_test/Django_test/mytestweb/test.py
@@ -2,17 +2,20 @@ import requests
 import bs4
 import pandas as pd
 import re
-from fake_useragent import UserAgent
+# from fake_useragent import UserAgent
 import time
 
-try:
-    base_url = 'https://www.ptt.cc'
-    sub_url = '/bbs/Gossiping/index.html'
-    ua = UserAgent()
-    user_agent = ua.random
-    my_headers = {'user-agent':user_agent,'cookie': 'over18=1'}
 
-    data = {"category":[], "title":[], "pop":[], "author":[], "date":[]}
+# 預設爬蟲參數
+base_url = 'https://www.ptt.cc'
+sub_url = '/bbs/Gossiping/index.html'
+# ua = UserAgent()
+# user_agent = ua.random
+my_headers = {'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36 Edg/123.0.0.0','cookie': 'over18=1'}
+
+# 迴圈重複抓取PPT文章
+data = {"category":[], "title":[], "pop":[], "author":[], "date":[]}
+try:
     while True:
         full_url = base_url + sub_url
         response_url = requests.get(full_url, headers = my_headers)
@@ -20,29 +23,35 @@ try:
         ppt_articles = soup.find_all('div', class_='r-ent')
         # time.sleep(2)
 
-
+        # 迴圈文字處理各項欄位
         for article in ppt_articles:
             htmal_tag = article.find('div', class_='title').text.strip('')
-
+    
+            # 取得文章標題
             title = htmal_tag.replace('[問卦]', '').replace('[新聞]', '').replace('[爆卦]', '').strip()
-
+    
+            # 取得文章類別
             match = re.search(r'\[.*?\]', htmal_tag)
             if match:
                 category = match.group(0)
             
+            # 過濾公告或協尋文章
             if '[公告]' in htmal_tag or '[協尋]' in htmal_tag:
                 continue
-
+    
+            # 取得回文數
             pop = article.find('div', class_='nrec').text.strip()
             
+            # 取得作者ID
             author = article.find('div', class_='author').text.strip()
             
+            # 日期字串拆分處理
             date_str = article.find('div', class_='date').text.strip()
             month, day  = date_str.split('/')
             date = int(str(int(month)) + str(int(day[0])) + str(int(day[1])))
             # print(date)
             
-
+            # 資料回填到data字典,並轉pandas df格式
             data["category"].append(category)
             data["title"].append(title)
             data["pop"].append(pop)
@@ -50,30 +59,36 @@ try:
             data["date"].append(date_str)
             pandas_dataframe = pd.DataFrame(data)
             # print(pandas_dataframe)
-            # print(len(pandas_dataframe))
+            print("Date: ",date, "Index: ", len(pandas_dataframe))
+                
+    
+            # 尋找tag 'a'，並找到上一頁的網址
+            prev_link = soup.find('a', string='‹ 上頁')
+            if prev_link is None:
+                break
+            else:
+                sub_url = prev_link['href']
+    
+            # 判斷日期是101時則結束迴圈
+            if date == 506:
+                print(f'Break === date is {date}')
+                break
 
 
-        # 尋找tag 'a'，並找到上一頁的網址
-        prev_link = soup.find('a', string='‹ 上頁')
-        if prev_link is None:
-            break
-        else:
-            sub_url = prev_link['href']
 
-            
-        if date == 101:
-            print('Break === date is 505')
-            break     
-
+        pandas_dataframe.to_csv(r'C:/Users/USER/Desktop/PTT_Gossiping_data.csv', 
+                                        encoding='utf-8', 
+                                        index=False)
+        print("========Write======")
+        print(sub_url)
+        
+    
+# 異常發生回傳full_url並在嘗試執行迴圈
 except Exception as e:
-    print(e)
-
-finally:
-    pandas_dataframe.to_csv(r'/Users/leo/Python_Django/Python_Django/django_test/Django_test/mytestweb/PTT_Gossiping_data.csv', 
-                                    encoding='utf-8', 
-                                    index=False)
-    print("========Write======")
-    print(sub_url)
+    print("while True error: ",e)
+    print(f"Current sub_url: {sub_url}")
+    full_url = base_url + sub_url
+    
     
         # pandas_dataframe.to_csv(r'C:\Users\USER\PTT_Gossiping_data.csv', 
         #                         encoding='utf-8', 
@@ -92,5 +107,3 @@ finally:
         #         print(count_date)
         #         print("========DONE======")
         #         return
-
-
